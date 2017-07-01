fca164aef56b1ebb0abccbd733e7431a4ca94af8
jieming.liu <jiemmy.liu@gmail.com>
2017-07-01 18:43:16 +0100

    initial spike

--- app/src/app.js
+++ app/src/app.js
@@ -11,29 +11,30 @@ import Users from 'src/users/Users';
 import ClaimantController from 'src/claimant/ClaimantController';
 
 export default angular.module('starter-app', ['ngMaterial', Users.name, 'ngRoute'])
-  .config(($mdIconProvider, $mdThemingProvider, $routeProvider) => {
-    // Register the user `avatar` icons
-    $mdIconProvider
-      .defaultIconSet("./assets/svg/avatars.svg", 128)
-      .icon("menu", "./assets/svg/menu.svg", 24)
-      .icon("share", "./assets/svg/share.svg", 24)
-      .icon("google_plus", "./assets/svg/google_plus.svg", 24)
-      .icon("hangouts", "./assets/svg/hangouts.svg", 24)
-      .icon("twitter", "./assets/svg/twitter.svg", 24)
-      .icon("phone", "./assets/svg/phone.svg", 24);
+    .config(($mdIconProvider, $mdThemingProvider, $routeProvider) => {
+        // Register the user `avatar` icons
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu", "./assets/svg/menu.svg", 24)
+            .icon("share", "./assets/svg/share.svg", 24)
+            .icon("google_palus", "./assets/svg/google_plus.svg", 24)
+            .icon("hangouts", "./assets/svg/hangouts.svg", 24)
+            .icon("twitter", "./assets/svg/twitter.svg", 24)
+            .icon("phone", "./assets/svg/phone.svg", 24);
 
-    $routeProvider
-      .when("/claimant", {
-        templateUrl: "src/claimant/claimant.tpl.html",
-        controller: ClaimantController
-      })
-      .otherwise({
-        redirectTo: '/#'
-      });
+        $routeProvider
+            .when("/claimant", {
+                templateUrl: "src/claimant/claimant.tpl.html",
+                controller: ClaimantController,
+                controllerAs: "vm"
+            })
+            .otherwise({
+                redirectTo: '/#'
+            });
 
-    $mdThemingProvider.theme('default')
-      .primaryPalette('indigo')
-      .accentPalette('teal');
-  })
-  .controller('AppController', AppController)
-  .controller('ClaimantController', ClaimantController);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('indigo')
+            .accentPalette('teal');
+    })
+    .controller('AppController', AppController)
+    .controller('ClaimantController', ClaimantController);
--- app/src/claimant/ClaimantController.js
+++ app/src/claimant/ClaimantController.js
@@ -1,4 +1,28 @@
 function ClaimantController() {
+    var vm = this;
+
+    vm.message = 'fsdf';
+
+    vm.inCreateMode = false;
+    vm.enterCreateClaimMode = enterCreateClaimMode;
+    vm.saveClaim = saveClaim;
+    vm.discardClaim = discardClaim;
+    vm.tabs = [
+        { title: 'Step One - Claimee', content: "Who is your claim against?"},
+        { title: 'Step Two - Confirm Claimee', content: "Is this them?", subContent: "Bill’s Kitchens Ltd, 1 High Street, Trumpington, Noshire NN1 2TT, Company number 987654321"},
+    ];
+
+    function enterCreateClaimMode() {
+        vm.inCreateMode = true;
+    }
+
+    function saveClaim() {
+        vm.inCreateMode = false;
+    }
+
+    function discardClaim() {
+        vm.inCreateMode = false;
+    }
 }
 
 export default [ClaimantController];
