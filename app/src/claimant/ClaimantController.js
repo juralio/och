6e52a02d1a0e7f814efc7a944be757d0b316167e
Richie Mackay <richie.mackay@gmail.com>
2017-07-02 00:27:42 +0100

    add fake upload

--- app/src/claimant/ClaimantController.js
+++ app/src/claimant/ClaimantController.js
@@ -1,78 +1,88 @@
 function ClaimantController($rootScope) {
-    var vm = this;
+  const vm = this;
 
-    $rootScope.username = 'Martin Smith';
+  $rootScope.username = 'Martin Smith';
 
-    vm.inCreateMode = false;
-    vm.enterCreateClaimMode = enterCreateClaimMode;
-    vm.confirmClaimee = confirmClaimee;
-    vm.discardClaimee = discardClaimee;
-    vm.searchClaimee = searchClaimee;
-    vm.confirmProblem = confirmProblem;
+  vm.inCreateMode = false;
+  vm.enterCreateClaimMode = enterCreateClaimMode;
+  vm.confirmClaimee = confirmClaimee;
+  vm.discardClaimee = discardClaimee;
+  vm.searchClaimee = searchClaimee;
+  vm.confirmProblem = confirmProblem;
+  vm.displayUploadMessage = () => {
+    console.log($rootScope.fileUploaded);
+    return $rootScope.fileUploaded;
+  };
+  vm.uploadFile = () => {
+    $rootScope.fileUploaded = true;
+    $rootScope.$apply();
+  };
+
+  vm.locks = {
+    firstTabLock: false,
+    secondTabLock: true,
+    thirdTabLock: true
+  };
+  vm.activeStatus = {
+    firstTabActive: true,
+    secondTabActive: false,
+    thirdTabActive: false,
+  };
+
+  vm.tabs = [
+    { title: 'Step One - Claimee', content: "Who is your claim against?" },
+    {
+      title: 'Step Two - Confirm Claimee',
+      content: "Is this them?",
+      subContent: "Bill’s Kitchens Ltd, 1 High Street, Trumpington, Noshire NN1 2TT, Company number 987654321"
+    },
+  ];
+
+  function enterCreateClaimMode() {
+    vm.inCreateMode = true;
+  }
+
+  function confirmClaimee() {
+    vm.locks = {
+      firstTabLock: false,
+      secondTabLock: false,
+      thirdTabLock: true
+    };
+
+    vm.activeStatus = {
+      firstTabActive: false,
+      secondTabActive: true,
+      thirdTabActive: false,
+    };
+  }
+
+  function discardClaimee() {
+    vm.displayClaimee = false;
 
     vm.locks = {
-        firstTabLock: false,
-        secondTabLock: true,
-        thirdTabLock: true
+      firstTabLock: false,
+      secondTabLock: true,
+      thirdTabLock: true
     };
+  }
+
+  function searchClaimee() {
+    vm.displayClaimee = true;
+  }
+
+  function confirmProblem() {
+    vm.locks = {
+      firstTabLock: false,
+      secondTabLock: false,
+      thirdTabLock: false
+    };
+
     vm.activeStatus = {
-        firstTabActive: true,
-        secondTabActive: false,
-        thirdTabActive: false,
+      firstTabActive: false,
+      secondTabActive: false,
+      thirdTabActive: true,
     };
-
-
-
-    vm.tabs = [
-        { title: 'Step One - Claimee', content: "Who is your claim against?"},
-        { title: 'Step Two - Confirm Claimee', content: "Is this them?", subContent: "Bill’s Kitchens Ltd, 1 High Street, Trumpington, Noshire NN1 2TT, Company number 987654321"},
-    ];
-
-    function enterCreateClaimMode() {
-        vm.inCreateMode = true;
-    }
-
-    function confirmClaimee() {
-        vm.locks = {
-            firstTabLock: false,
-            secondTabLock: false,
-            thirdTabLock: true
-        };
-
-        vm.activeStatus = {
-            firstTabActive: false,
-            secondTabActive: true,
-            thirdTabActive: false,
-        };
-    }
-
-    function discardClaimee() {
-        vm.displayClaimee = false;
-
-        vm.locks = {
-            firstTabLock: false,
-            secondTabLock: true,
-            thirdTabLock: true
-        };
-    }
-
-    function searchClaimee() {
-        vm.displayClaimee = true;
-    }
-
-    function confirmProblem() {
-        vm.locks = {
-            firstTabLock: false,
-            secondTabLock: false,
-            thirdTabLock: false
-        };
-
-        vm.activeStatus = {
-            firstTabActive: false,
-            secondTabActive: false,
-            thirdTabActive: true,
-        };
-    }
+  }
 }
 
 export default ["$rootScope", ClaimantController];
