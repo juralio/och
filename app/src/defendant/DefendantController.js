a1b71122ecfb37832957b107b50d9cae1826b5d0
bob-pack <bob.pack@juralio.com>
2017-07-02 02:48:09 +0100

    Almost working with no options

--- app/src/defendant/DefendantController.js
+++ app/src/defendant/DefendantController.js
@@ -5,6 +5,10 @@ function DefendantController($rootScope) {
 
   vm.sendResponse = sendResponse;
   vm.next = next;
+  vm.showClaimText = showClaimText;
+  vm.showClaimVideo = showClaimVideo;
+  vm.showSolutionText = showSolutionText();
+  vm.showSolutionVideo = showSolutionVideo();
 
   vm.locks = {
     firstTabLock: false,
@@ -15,6 +19,16 @@ function DefendantController($rootScope) {
     firstTabActive: true,
     secondTabActive: false,
   };
+
+  vm.disagreeClaim = {
+    textVisible: true,
+    videoVisible: false,
+  }
+
+  vm.disagreeSolution = {
+    textVisible: true,
+    videoVisible: false,
+  }
 
   vm.tabs = [
     { content: "Claim brought against you by Martin Smith.", summary: "This is some summary information", videoLink: "./assets/taps.mp4"},
@@ -35,6 +49,34 @@ function DefendantController($rootScope) {
 
   function sendResponse() {
     alert("you clicked send");
+  }
+
+  function showClaimText() {
+    vm.disagreeClaim = {
+      textVisible: true,
+      videoVisible: false,
+    };
+  }
+
+  function showClaimVideo() {
+    vm.disagreeClaim = {
+      textVisible: false,
+      videoVisible: true,
+    };
+  }
+
+  function showSolutionText() {
+    vm.disagreeSolution = {
+      textVisible: true,
+      videoVisible: false,
+    };
+  }
+
+  function showSolutionVideo() {
+    vm.disagreeSolution = {
+      textVisible: false,
+      videoVisible: true,
+    };
   }
 }
 
