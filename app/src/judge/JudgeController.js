5b5b9d06a54f25d5da45ee39e5fa62f38f9936c1
jieming.liu <jiemmy.liu@gmail.com>
2017-07-02 03:58:22 +0100

    Add basic Judge structure

--- app/src/judge/JudgeController.js
+++ app/src/judge/JudgeController.js
@@ -1,8 +1,12 @@
 function DefendantController($rootScope) {
+    var vm = this;
 
     $rootScope.username = 'Judge Khan';
 
-
+    vm.lhp = {
+        problemDescription: "Bill Kitchen Ltd promised me gold tabs, but it is silver!",
+        problemSolution: "I need claiming £5000 from Bill Kitchen to replace the tabs."
+    };
 }
 
 export default ["$rootScope", DefendantController];
